b6555bf2fc9a4c81c7555097af96105707962ded
bessemouelhi <bessem.mouelhi@outlook.fr>
2019-08-08 12:02:43 +0200

    add sign-in form

--- src/main/java/com/bessem/controller/Main.java
+++ src/main/java/com/bessem/controller/Main.java
@@ -1,15 +1,30 @@
 package com.bessem.controller;
 
 import org.apache.log4j.BasicConfigurator;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
         port(3000);
         BasicConfigurator.configure();
-        get("/home", (request, response) -> "Welcome Everybody!");
+
+        Map map = new HashMap();
+        map.put("username", "Bessem");
+
+        // index.hbs file is in resources/templates directory
+        get("/", (req, res) -> new ModelAndView(map, "index.hbs"),
+                new HandlebarsTemplateEngine());
+
+        post("/sign-in", (req, res) -> {
+            map.put("username", req.queryParams("username"));
+            return new ModelAndView(map, "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
