df022a120f26d3a771dbf2bf247118c5fea37e77
bessemouelhi <bessem.mouelhi@outlook.fr>
2019-08-14 18:11:22 +0200

    add flash message

--- src/main/java/com/bessem/controller/Main.java
+++ src/main/java/com/bessem/controller/Main.java
@@ -6,6 +6,7 @@ import com.bessem.model.CourseIdeaImpl;
 import com.bessem.model.CourseIdeaNotFoundException;
 import org.apache.log4j.BasicConfigurator;
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
@@ -15,6 +16,9 @@ import static spark.Spark.*;
 
 public class Main {
 
+    private static final String FLASH_MESSAGE_KEY = "flash_message";
+    private static final String MODEL_KEY = "model_key";
+
     public static void main(String[] args) {
         port(3000);
         BasicConfigurator.configure();
@@ -22,16 +26,19 @@ public class Main {
 
         CourseIdeaDao ideaDao = new CourseIdeaImpl();
 
-        Map map = new HashMap();
+        Map<String, Object> model = new HashMap<>();
 
         before((req, res) -> {
             if (req.cookie("username") != null) {
                 req.attribute("username", req.cookie("username"));
             }
+            model.put("flashMessage", captureFlashMessage(req));
+            req.attribute(MODEL_KEY, model);
         });
 
         before("/ideas", (req, res) -> {
             if (req.attribute("username") == null) {
+                setFlashMessage(req, "Whoops, please sign in first!");
                 res.redirect("/");
                 halt();
             }
@@ -39,20 +46,19 @@ public class Main {
 
         // index.hbs file is in resources/templates directory
         get("/", (req, res) -> {
-            map.put("username", req.attribute("username"));
-            return new ModelAndView(map, "index.hbs");
+            model.put("username", req.attribute("username"));
+            return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/sign-in", (req, res) -> {
             String username = req.queryParams("username");
-            map.put("username", username);
+            model.put("username", username);
             res.cookie("username", username);
             res.redirect("/");
             return null;
         });
 
         get("/ideas", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
             model.put("ideas", ideaDao.getAll());
             return new ModelAndView(model, "ideas.hbs");
         }, new HandlebarsTemplateEngine());
@@ -69,7 +75,6 @@ public class Main {
 
         get("/ideas/:slug", (req, res) -> {
             CourseIdea idea = ideaDao.findBySlug(req.params("slug"));
-            Map<String, Object> model = new HashMap<>();
             model.put("idea", idea);
 
             return new ModelAndView(model, "idea.hbs");
@@ -78,14 +83,18 @@ public class Main {
         post("/ideas/:slug/vote", (req, res) -> {
             CourseIdea idea = ideaDao.findBySlug(req.params("slug"));
             //System.out.println(idea.getAuthor());
-            idea.vote(req.attribute("username"));
+            boolean added = idea.vote(req.attribute("username"));
+            if (added) {
+                setFlashMessage(req, "Thanks for your vote!");
+            } else {
+                setFlashMessage(req, "You already voted!");
+            }
             res.redirect("/ideas");
             return null;
         });
 
         exception(CourseIdeaNotFoundException.class, (exc, req, res) -> {
             res.status(404);
-            Map<String, Object> model = new HashMap<>();
             model.put("exc", exc.getMessage());
             HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
             String html = engine.render(
@@ -93,5 +102,27 @@ public class Main {
             );
             res.body(html);
         });
+    }
+
+    private static void setFlashMessage(Request req, String message) {
+        req.session().attribute(FLASH_MESSAGE_KEY, message);
+    }
+
+    private static String getFlashMessage(Request req) {
+        if (req.session(false) == null) {
+            return null;
+        }
+        if (!req.session().attributes().contains(FLASH_MESSAGE_KEY)) {
+            return null;
+        }
+        return (String) req.session().attribute(FLASH_MESSAGE_KEY);
+    }
+
+    private static String captureFlashMessage(Request req) {
+        String message = getFlashMessage(req);
+        if (message != null) {
+            req.session().removeAttribute(FLASH_MESSAGE_KEY);
+        }
+        return message;
     }
 }
